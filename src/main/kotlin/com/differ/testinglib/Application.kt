8b6ab56c2350704192dc8be146ec016a1a055b15
v.iusiumbeli <slavayus@gmail.com>
2020-02-03 23:49:20 +0300

    add differ library and plugin

--- src/main/kotlin/com/differ/testinglib/Application.kt
+++ src/main/kotlin/com/differ/testinglib/Application.kt
@@ -1,11 +1,13 @@
 package com.differ.testinglib
 
+import com.differ.differcore.annotations.EnableDiffer
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableDiffer
 open class Application
 
 fun main(args: Array<String>) {
